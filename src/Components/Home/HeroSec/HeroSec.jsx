d7e630af6dad1ae2c50a44d6310ac6a87437084d
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2022-03-24 04:21:19 +0530

    Time horizontally aligned in the venue card

--- src/Components/Home/HeroSec/HeroSec.jsx
+++ src/Components/Home/HeroSec/HeroSec.jsx
@@ -26,7 +26,7 @@ const HeroSec = () => {
             <div className='col-12 col-md-3 py-3 px-5 time-card card2'>
                 <div className='venue mt-3'>                    
                     <span style={{fontSize: "1.8rem", fontWeight: "500"}}>Time</span>
-                    <div className='col-auto d-flex align-items-center'>
+                    <div className='col-auto d-flex align-items-center justify-content-center'>
                         <div className='col-4 p-3 text-center time-block' style={{background: '#022649', borderRadius: '1rem'}}>
                             <div className='col-12' style={{color: 'white', fontSize: '1.75rem'}}>9:00</div>
                             <div className='col-12' style={{color: 'white'}}>AM</div>
